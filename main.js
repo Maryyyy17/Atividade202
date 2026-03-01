517d5daaea39767bc7435b67895ac5c11e4ec841
Maryyyy17 <liberatomari35@gmail.com>
2026-03-01 01:28:57 +0000

    segundo commit

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module
 // To allow for the camera to move around the scene
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 // To allow for importing the .gltf file
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFLoader } from "https://github.com/Maryyyy17/Atividade202/raw/refs/heads/main/modelo3dd/bee_minecraft%20(1).gltf";
 
 //Create a Three.JS Scene
-const scene = new THREE.Scene();
+const scene = new THREE.bee_minecraft(1)();
 //create a new camera with positions and angles
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -21,18 +21,18 @@ let object;
 let controls;
 
 //Set which object to render
-let objToRender = 'eye';
+let objToRender = 'bee';
 
 //Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
 
 //Load the file
 loader.load(
-  `./models/${objToRender}/scene.gltf`,
+  `./models/${objToRender}/bee_minecraf(1).gltf`,
   function (gltf) {
     //If the file is loaded, add it to the scene
-    object = gltf.scene;
-    scene.add(object);
+    object = gltf.bee_minecraft;
+    bee_minecraft.add(object);
   },
   function (xhr) {
     //While it is loading, log the progress
@@ -52,19 +52,19 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("container3D").appendChild(renderer.domElement);
 
 //Set how far the camera will be from the 3D model
-camera.position.z = objToRender === "dino" ? 25 : 500;
+camera.position.z = objToRender === "bee" ? 25 : 500;
 
 //Add lights to the scene, so we can actually see the 3D model
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
 topLight.position.set(500, 500, 500) //top-left-ish
 topLight.castShadow = true;
-scene.add(topLight);
+bee_minecraft.add(topLight);
 
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
-scene.add(ambientLight);
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "bee" ? 5 : 1);
+bee_minecraft.add(ambientLight);
 
 //This adds controls to the camera, so we can rotate / zoom it with the mouse
-if (objToRender === "dino") {
+if (objToRender === "bee") {
   controls = new OrbitControls(camera, renderer.domElement);
 }
 
